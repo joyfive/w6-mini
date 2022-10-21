ec463659251bc102e0ac545e96ce19561ac23659
joyfive <joy_five@kakao.com>
2022-10-21 23:21:33 +0900

    first setting & button component

---
+++ src/components/elements/Input.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const Input = (props) => {
+  return (
+    <Input {...props} disabled={props.disabled}>
+      {props.children}
+    </Input>
+  );
+}
+
+const Input = styled.button`
+  padding: 10px;
+  margin: 5px 5px;
+  align-self: center;
+  width: 240px;
+  height: 40px !important;
+  font-weight: 600;
+  font-size: 1rem;
+  line-height: 1.6;
+  border: 0;
+  color: #fd5c63;
+  border-radius: 10px;
+  box-shadow: 0px 2px 10px #e1cccd;
+  background-color: white;
+  cursor: pointer;
+&:hover {
+  background-color: #fd5c63;
+  border: 0;
+  color: white;
+}
+${({ size }) => {
+    switch (size) {
+      case "full":
+        return css`
+          width: 100%;
+        `;
+      case "large":
+        return css`
+          width: 50%;
+        `;
+      case "medium":
+        return css`
+          width: 200px;
+          height: 40px !important;
+          padding; 5px;
+        `
+      case "small":
+        return css`
+          width: 40px;
+          height: 40px !important;
+          padding: 10px;
+        `;
+      case "round":
+        return css`
+        width: 60px;
+        height: 60px !important;
+        padding: 20px;
+        border-radius: 50px; 
+        `;
+      default:
+      return css`
+        width: 240px;
+        height: 40px !important;
+        padding: 10px;
+      `;
+    }
+  }
+}
+${({ color }) => {
+  switch (color) {
+    case "reverse":
+      return css`
+          color: white;
+          background-color: #fd5c63;
+        &:hover {
+          background-color: white;
+          border: 0;
+          color: #fd5c63;
+      
+      }
+      `
+      case "line":
+      retrun css`
+        color: white;
+        border: 1px solid #fd5c63;
+      `
+      
+    default:
+      return css`
+          color: #fd5c63;
+          background-color: white;
+        &:hover {
+          background-color: #fd5c63;
+          border: 0;
+          color: white;
+      
+      }
+      `
+}
+}
+}
+`
---
+++ src/shared/Router.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "../pages/Home"
+import Editor from "../pages/Editor";
+import Post from "../pages/Post";
+import AllList from "../pages/AllList";
+import TeamList from "../pages/TeamList";
+import Signin from "../pages/Signin";
+import Signup from "../pages/Signup";
+import Mypage from "../pages/Mypage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 
+						Routes안에 이렇게 작성합니다. 
+						Route에는 react-router-dom에서 지원하는 props들이 있습니다.
+
+						paht는 우리가 흔히 말하는 사용하고싶은 "주소"를 넣어주면 됩니다.
+						element는 해당 주소로 이동했을 때 보여주고자 하는 컴포넌트를 넣어줍니다.
+				 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<AllList />} />
+        <Route path="/list/team" element={<TeamList />} />
+        <Route path="/Editor" element={<Editor />} />
+        <Route path="/posts/:id" element={<Post />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
+
